0813ae5c638da9c7a3f9865a5afdd19c70722c79
avandamhuis-cmd <avandamhuis@gmail.com>
2026-06-17 19:48:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const keys = {};
-const mouse = {
-    x: canvas.width / 2,
-    y: canvas.height / 2
-};
+const mouse = { x: 0, y: 0 };
 
 const particles = [];
 
@@ -37,6 +34,7 @@ const jet = {
 };
 
 function update() {
+
     const targetAngle = Math.atan2(
         mouse.y - jet.y,
         mouse.x - jet.x
@@ -50,6 +48,7 @@ function update() {
     jet.angle += diff * 0.08;
 
     if (keys["w"]) {
+
         jet.vx += Math.cos(jet.angle) * jet.thrust;
         jet.vy += Math.sin(jet.angle) * jet.thrust;
 
@@ -57,16 +56,9 @@ function update() {
             particles.push({
                 x: jet.x - Math.cos(jet.angle) * 40,
                 y: jet.y - Math.sin(jet.angle) * 40,
-
-                vx:
-                    -Math.cos(jet.angle) * (4 + Math.random() * 2) +
-                    (Math.random() - 0.5) * 0.8,
-
-                vy:
-                    -Math.sin(jet.angle) * (4 + Math.random() * 2) +
-                    (Math.random() - 0.5) * 0.8,
-
-                size: 3 + Math.random() * 3,
+                vx: -Math.cos(jet.angle) * 4,
+                vy: -Math.sin(jet.angle) * 4,
+                size: 3,
                 life: 1
             });
         }
@@ -93,116 +85,33 @@ function update() {
 
         p.life -= 0.04;
 
-        if (p.life <= 0) {
-            particles.splice(i, 1);
-        }
+        if (p.life <= 0) particles.splice(i, 1);
     }
 
     if (jet.x < -50) jet.x = canvas.width + 50;
     if (jet.x > canvas.width + 50) jet.x = -50;
-
     if (jet.y < -50) jet.y = canvas.height + 50;
     if (jet.y > canvas.height + 50) jet.y = -50;
+
+    updateWeapons(jet, keys);
 }
 
 function drawParticles() {
+
     for (const p of particles) {
-        if (Math.random() > 0.5) {
-            ctx.fillStyle = `rgba(255,220,0,${p.life})`;
-        } else {
-            ctx.fillStyle = `rgba(255,120,0,${p.life})`;
-        }
-
-        ctx.fillRect(
-            p.x - p.size / 2,
-            p.y - p.size / 2,
-            p.size,
-            p.size
-        );
+        ctx.fillStyle = `rgba(255,140,0,${p.life})`;
+        ctx.fillRect(p.x, p.y, p.size, p.size);
     }
-}
-
-function drawJet() {
-    ctx.save();
-
-    ctx.translate(jet.x, jet.y);
-    ctx.rotate(jet.angle);
-
-    // Main fuselage
-    ctx.fillStyle = "#c7ccd4";
-    ctx.beginPath();
-    ctx.moveTo(38, 0);
-    ctx.lineTo(16, -5);
-    ctx.lineTo(-22, -5);
-    ctx.lineTo(-36, -3);
-    ctx.lineTo(-42, 0);
-    ctx.lineTo(-36, 3);
-    ctx.lineTo(-22, 5);
-    ctx.lineTo(16, 5);
-    ctx.closePath();
-    ctx.fill();
-
-    // Connected wings
-    ctx.fillStyle = "#b8bec8";
-
-    ctx.beginPath();
-    ctx.moveTo(12, -3);
-    ctx.lineTo(-6, -18);
-    ctx.lineTo(-30, -24);
-    ctx.lineTo(-12, -2);
-    ctx.closePath();
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(12, 3);
-    ctx.lineTo(-6, 18);
-    ctx.lineTo(-30, 24);
-    ctx.lineTo(-12, 2);
-    ctx.closePath();
-    ctx.fill();
-
-    // Horizontal stabilizers
-    ctx.fillStyle = "#adb5c0";
-
-    ctx.beginPath();
-    ctx.moveTo(-24, -5);
-    ctx.lineTo(-36, -13);
-    ctx.lineTo(-30, -4);
-    ctx.closePath();
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(-24, 5);
-    ctx.lineTo(-36, 13);
-    ctx.lineTo(-30, 4);
-    ctx.closePath();
-    ctx.fill();
-
-    // Cockpit
-    ctx.fillStyle = "#6fd9ff";
-    ctx.beginPath();
-    ctx.ellipse(12, 0, 7, 3.5, 0, 0, Math.PI * 2);
-    ctx.fill();
-
-    // Engine nozzle
-    ctx.fillStyle = "#444";
-    ctx.fillRect(-44, -2, 4, 4);
-
-    // Afterburner
-    if (keys["w"]) {
-        ctx.fillStyle = `rgba(255,180,0,${0.6 + Math.random() * 0.3})`;
-        ctx.fillRect(-48, -2, 5, 4);
-    }
-
-    ctx.restore();
 }
 
 function draw() {
+
     ctx.fillStyle = "#4da6ff";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     drawParticles();
-    drawJet();
+    drawWeapons(ctx);
+    drawJet(ctx, jet, keys);
 }
 
 function gameLoop() {
